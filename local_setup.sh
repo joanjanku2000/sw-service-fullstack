f1fa9326d6536fde816a76e44a7c8af24cb05480
joanjanku2000 <janku.joan4@gmail.com>
2025-04-13 23:47:20 +0200

    add build phase

--- local_setup.sh
+++ local_setup.sh
@@ -3,6 +3,7 @@
 # Start Mongo container
 docker compose up mongo_db -d
 docker build -f service-app-fe/Dockerfile.local -t fe-app .
+docker compose build app
 docker compose run -e SPRING_PROFILES_ACTIVE=local -p 8181:8080 -d app
 docker run -p 4200:4200 -d fe-app
 
