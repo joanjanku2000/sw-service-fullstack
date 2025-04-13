3879a0cb815abf9a44e6d6543d3389368d9790b3
joanjanku2000 <janku.joan4@gmail.com>
2025-04-13 23:22:40 +0200

    update docker config to specify network name

--- local_setup.sh
+++ local_setup.sh
@@ -4,6 +4,6 @@
 docker compose up mongo_db -d
 docker build -f service-app-fe/Dockerfile.local -t fe-app .
 docker compose run -e SPRING_PROFILES_ACTIVE=local -p 8181:8080 -d app
-docker run --network=service-app_default -p 4200:4200 -d fe-app
+docker run --network=service-app_sw-network -p 4200:4200 -d fe-app
 
 
