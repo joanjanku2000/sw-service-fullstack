4c97c77a33ad08e4c9929121effefd34fea88f1a
joanjanku2000 <janku.joan4@gmail.com>
2025-04-13 23:14:55 +0200

    update the local_script to not depend on host machine having node or java & maven installed - DOCKER

--- local_setup.sh
+++ local_setup.sh
@@ -2,33 +2,8 @@
 
 # Start Mongo container
 docker compose up mongo_db -d
+docker build -f service-app-fe/Dockerfile.local -t fe-app .
+docker compose run -e SPRING_PROFILES_ACTIVE=local -p 8181:8080 -d app
+docker run --network=service-app_default -p 4200:4200 -d fe-app
 
-# kills underlying processes
-cleanup() {
-  echo "Cleaning up..."
-  if [[ -n "$SPRING_PID" ]]; then
-    kill "$SPRING_PID" 2>/dev/null && echo "Terminated Spring Boot (PID: $SPRING_PID)"
-  fi
-  if [[ -n "$NG_SERVE_PID" ]]; then
-    kill "$NG_SERVE_PID" 2>/dev/null && echo "Terminated Angular (PID: $NG_SERVE_PID)"
-  fi
 
-  docker compose down mongo_db
-  exit 0
-}
-
-# Trap (Ctrl+C), TERM, and EXIT signals
-trap cleanup INT TERM EXIT
-
-cd service-app
-mvn clean install -DskipTests=true
-mvn spring-boot:run &
-SPRING_PID=$!
-cd ..
-
-cd service-app-fe
-ng serve &
-NG_SERVE_PID=$!
-
-wait $SPRING_PID
-wait $NG_SERVE_PID
